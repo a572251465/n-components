037310851fdd3fd178d6ee653620a8b35d014b34
lihaohao <2495041749@qq.com>
2023-04-12 12:14:10 +0800

    feat: n-message params 类型合并

---
+++ packages/n-message/index.ts
@@ -0,0 +1,5 @@
+import { NMessage, useGlobalMessageField } from "./src/message-instance";
+export * from "./src/types";
+export { NMessage, useGlobalMessageField };
+
+export default NMessage;
--- packages/n-message/src/message-instance.ts
+++ packages/n-message/src/message-instance.ts
@@ -1,9 +1,49 @@
-import { IPartFn, ITheme, IType, NMessageProps } from "./types";
+import {
+  IMessageProps,
+  INormalFn,
+  IPartFn,
+  ITheme,
+  IType,
+  NMessageProps,
+} from "./types";
+type IMergeType = Omit<IMessageProps, "theme" | "type">;
+
+let globalConfigField: Partial<IMergeType> = {};
+export const useGlobalMessageField = (mergeInfo: IMergeType) => {
+  globalConfigField = mergeInfo;
+};
+
+const defaultFn = Function.prototype as INormalFn;
+const defaultValues: IMessageProps = {
+  duration: 3000,
+  theme: "light",
+  message: "",
+  type: "success",
+  showClose: true,
+  autoClose: true,
+  align: "left",
+  onClose: defaultFn,
+  offset: 16,
+};
 
 const handleMessage =
   (info: { type: IType; theme: ITheme } | IType): IPartFn =>
   (message) => {
-    console.log(info, message);
+    const mergeInfo = (
+      typeof info === "string" ? { type: info, theme: "light" } : info
+    ) as { type: IType; theme: ITheme };
+    const mergeOptions = Object.assign(
+      mergeInfo,
+      typeof message === "string" ? { message } : message
+    ) as Partial<IMessageProps>;
+    const finalOptions = Object.assign(
+      {},
+      defaultValues,
+      globalConfigField,
+      mergeOptions
+    ) as IMessageProps;
+
+    console.log(finalOptions);
   };
 
 const handleProps = (theme?: ITheme) => {
